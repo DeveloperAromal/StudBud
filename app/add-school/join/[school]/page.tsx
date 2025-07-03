6ccbc863029ecb5bfd8c8cdc87e714f564c113bf
Aromal <aromals161@gmail.com>
2025-07-03 13:41:41 +0530

    fix

--- app/add-school/join/[school]/page.tsx
+++ app/add-school/join/[school]/page.tsx
@@ -1,11 +1,23 @@
-import JoinLinkClient from "../../widgets/JoinLinkClient"; // import the client component
-export default async function JoinLink({
-  params,
-}: {
-  params: { school: string };
-}) {
+// app/add-school/join/[school]/page.tsx
+
+import JoinLinkClient from "../../widgets/JoinLinkClient";
+
+interface PageProps {
+  params: {
+    school: string;
+  };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+const rootDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN;
+
+export default function JoinLink({ params }: PageProps) {
   const { school } = params;
-  const url = `http://${school}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}:${process.env.PORT}`;
+
+  const isDev = process.env.NODE_ENV === "development";
+  const url = isDev
+    ? `http://${school}.${rootDomain}:${process.env.PORT}`
+    : `https://${school}.${rootDomain}`;
 
   return <JoinLinkClient url={url} />;
 }
