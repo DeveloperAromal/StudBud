5ba6edde3f2166224af69a1995762601deedc24c
Aromal <aromals161@gmail.com>
2025-07-03 13:33:02 +0530

    fix

--- app/add-school/join/[school]/page.tsx
+++ app/add-school/join/[school]/page.tsx
@@ -1,32 +1,16 @@
-import axios from "axios";
-import JoinLinkClient from "../../widgets/JoinLinkClient";
-
-interface PageProps {
+import JoinLinkClient from "../../widgets/JoinLinkClient"; // import the client component
+interface Props {
   params: {
     school: string;
   };
-  // searchParams?: { [key: string]: string | string[] | undefined };
 }
 
 const rootDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN;
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
-export default async function JoinLink({ params }: PageProps) {
+export default async function JoinLink({ params }: Props) {
   const { school } = params;
 
-  let adminData: any;
-  try {
-    const response = await axios.get(
-      `${baseUrl}/api/v1/getSubdomainList/${school}`
-    );
-    adminData = response.data[0];
-  } catch (error) {
-    console.error("Error fetching subdomain list:", error);
-    return <div>Error loading school data.</div>;
-  }
-
-  // For production, don't include process.env.PORT in URL
-  const url = `http://${school}.${rootDomain}`;
+  const url = `http://${school}.${rootDomain}:${process.env.PORT}`;
 
   return <JoinLinkClient url={url} />;
 }
